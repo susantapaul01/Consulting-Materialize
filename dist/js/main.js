996ffb82e4197765a35c424189ac9f8fbed53bb6
Susanta paul <boxme.sp@gmail.com>
2021-10-26 11:08:15 +0600

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,8 @@
+M.AutoInit();
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, {
+        indicators: true,
+    });
+  });
